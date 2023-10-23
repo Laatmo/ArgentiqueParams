922842d0c27d6f81ac4788291e4e359714a30cb2
Latmo <tristan.robin@protonmail.com>
2023-10-23 14:17:19 +0200

    photo + list

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,8 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab2.css';
+import { Camera, CameraResultType } from '@capacitor/camera';
+import React, { useState } from 'react';
 
 const Tab2: React.FC = () => {
+
+  const [photoList, setPhotoList] = useState(new Array<string>);
+
+
+  const takePicture = async () => {
+    const image = await Camera.getPhoto({
+      quality: 90,
+      allowEditing: true,
+      resultType: CameraResultType.Uri
+    });
+
+     const imgArray: Array<string> = [];
+
+    if (undefined !== image.webPath) {
+      imgArray.push(image.webPath);
+      setPhotoList(photoList => ([...photoList, ...imgArray]));
+    }
+  };
+
   return (
     <IonPage>
       <IonHeader>
@@ -10,13 +30,25 @@ const Tab2: React.FC = () => {
           <IonTitle>Tab 2</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
+      <IonContent fullscreen onClick={() => takePicture()}>
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">Tab 2</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        {photoList.map((e: string) => {
+          return (
+          <IonCard>
+            <IonCardHeader>
+              <IonCardTitle>Card Title</IonCardTitle>
+              <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+            </IonCardHeader>
+          
+            <IonCardContent>
+              <img src={e}></img>
+            </IonCardContent>
+          </IonCard>)
+        })}
       </IonContent>
     </IonPage>
   );
